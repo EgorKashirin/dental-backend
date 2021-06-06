f9361754d8aaa389877741d7cc82c8c07387c994
Egor Kashirin <egor4815162342@yandex.ru>
2021-06-06 14:34:26 +0300

    Finall Back

--- controllers/AppointmentController.js
+++ controllers/AppointmentController.js
@@ -1,9 +1,9 @@
 const { validationResult } = require('express-validator');
-const { Appointment } = require('../models')
+const { Appointment,Patient } = require('../models')
 
 function AppointmentController () {}
 
-const create = function(req,res){
+const create = async function(req,res){
     const errors = validationResult(req);
     const data = {
         patient: req.body.patient,
@@ -22,6 +22,15 @@ const create = function(req,res){
         })
     }
 
+    try{
+        await Patient.findOne({ _id: data.patient});
+    }catch (e) {
+        return res.status(404).json({
+            success:false,
+            message: "PATIENT_NOT_FOUND",
+        })
+    }
+
     Appointment.create(data,function (err,doc) {
         if(err){
             return res.status(500).json({
@@ -35,6 +44,72 @@ const create = function(req,res){
             data: doc
         })
     });
+};
+
+const update = async function (req,res) {
+    const appointmentId = req.params.id;
+    const errors = validationResult(req);
+    const data = {
+        dentNumber : req.body.dentNumber,
+        diagnosis: req.body.diagnosis,
+        price: req.body.price,
+        date: req.body.date,
+        time: req.body.time,
+
+    }
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            success:false,
+            message: errors.array(),
+        })
+    }
+
+    Appointment.updateOne(
+        {_id:appointmentId},
+        {$set: data},
+        function (err,doc) {
+        if(err){
+            return res.status(500).json({
+                success:false,
+                message: err,
+            })
+        }
+        if(!doc){
+                return res.status(404).json({
+                    success:false,
+                    message: "APPOINTMENT_NOT_FOUND",
+                })
+        }
+
+        res.status(201).json({
+            success:true
+        })
+    });
+}
+
+const remove = async function (req,res) {
+    const id = req.params.id;
+    try{
+        await Appointment.findOne({ _id: id});
+    }catch (e) {
+            return res.status(404).json({
+                success:false,
+                message: "APPOINTMENT_NOT_FOUND",
+            })
+    }
+
+    Appointment.deleteOne({_id:id}),(err)=>{
+        if(err){
+            return res.status(500).json({
+                success:false,
+                message: err
+            })
+        }
+         res.json({
+            status: 'success',
+        })
+    };
 }
 
 const all = function (req,res) {
@@ -55,7 +130,7 @@ const all = function (req,res) {
 }
 
 AppointmentController.prototype = {
-    all,create
+    all,create,remove,update
 }
 
 module.exports = AppointmentController;
